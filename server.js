ea02c9713da83a6a0e1a711049f7b8859bdb68a3
Saurabh <saurabhkushwaha2030@gmail.com>
2024-03-05 23:36:26 +0530

    run bulid

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import colors from "colors";
 import dotenv from "dotenv";
@@ -7,6 +8,7 @@ import authRoutes from "./routes/authRoute.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
+
 
 //configure env
 dotenv.config();
@@ -22,10 +24,23 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
+
+
+const __dirname = path.resolve();
+
 //routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
+
+
+
+app.use(express.static(path.join(__dirname,"/client/build")));
+
+app.get("*" , (req,res)=>{
+  res.sendFile(path.join(__dirname,"client","build","index.html"));
+})
+
 
 //rest api
 app.get("/", (req, res) => {
@@ -34,6 +49,9 @@ app.get("/", (req, res) => {
 
 //PORT
 const PORT = process.env.PORT || 8080;
+
+
+
 
 //run listen
 app.listen(PORT, () => {
